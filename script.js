4f555dfd39a530242ad262173d973049c7c47d52
TheLoloS <57485545+TheLoloS@users.noreply.github.com>
2022-05-24 14:51:03 +0200

    1.1.6
    add data in forms

--- script.js
+++ script.js
@@ -8,15 +8,45 @@ window.addEventListener("popstate", function (event) {
   }
 });
 let showQRBtn = document.querySelector("#upBtn");
+let hideQRBtn = document.querySelector("#qr > button");
+let fDate = document.querySelector("#fDate");
+let busNum = document.querySelector("#busNum");
 
-showQRBtn.addEventListener("click", () => {});
+showQRBtn.addEventListener("click", () => {
+  document.getElementById("qr").style.display = "flex";
+});
+
+hideQRBtn.addEventListener("click", () => {
+  document.getElementById("qr").style.display = "none";
+});
+
+fDate.addEventListener("click", () => {
+  let answer = window.confirm("Napewno chcesz ustalić nową date?");
+  let d = new Date();
+  if (answer) {
+    fDate.innerHTML = `${d.toLocaleDateString(
+      "fr-CH"
+    )}&nbsp;godz.&nbsp;${d.toLocaleTimeString()}`;
+  } else {
+    //some code
+  }
+});
+
+busNum.addEventListener("click", () => {
+  let promptAnswer = window.prompt("Wpisz numer autobusu:");
+  if (promptAnswer) {
+    busNum.innerHTML = promptAnswer;
+  } else {
+    console.log("nie działa");
+  }
+});
 
 var myVar = setInterval(function () {
   myTimer();
 }, 1000);
 
 function myTimer() {
-  var d = new Date();
+  let d = new Date();
   document.getElementById(
     "clock"
   ).innerHTML = `${d.toLocaleTimeString()}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${d.toLocaleDateString(
