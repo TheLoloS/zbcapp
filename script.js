8056bd2c97e044a9f8c6f4a7d6de7056e68f561d
TheLoloS <57485545+TheLoloS@users.noreply.github.com>
2022-06-01 11:01:06 +0200

    1.2
    -add types of ticket
    -fix some bugs

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let fDate = document.querySelector("#fDate");
 let showQRBtn = document.querySelector("#upBtn");
 let hideQRBtn = document.querySelector("#qr > button");
 let busNum = document.querySelector("#busNum");
+let chooseBus = document.querySelector(".form-control");
 
 window.addEventListener("load", function () {
   window.history.pushState({ noBackExitsApp: true }, "");
@@ -10,6 +11,9 @@ window.addEventListener("load", function () {
     : null;
   localStorage.getItem("numberBus")
     ? (busNum.innerHTML = localStorage.getItem("numberBus"))
+    : null;
+  localStorage.getItem("selectedTicket")
+    ? changeTicket(Number(localStorage.getItem("selectedTicket")))
     : null;
 });
 
@@ -45,6 +49,10 @@ fDate.addEventListener("click", () => {
   }
 });
 
+chooseBus.addEventListener("change", (e, i) => {
+  changeTicket(Number(e.target.value));
+});
+
 busNum.addEventListener("click", () => {
   let promptAnswer = window.prompt("Wpisz numer autobusu:");
   if (promptAnswer) {
@@ -67,3 +75,99 @@ function myTimer() {
     "fr-CH"
   )}`;
 }
+
+const changeTicket = (selectedTicket) => {
+  let title1 = document.querySelector("h1");
+  let title2 = document.querySelector("#countB");
+  let title3 = document.querySelector("#countC");
+  let fotterText = document.querySelector("#fotterText");
+  title1.innerHTML = Ticket[selectedTicket].title;
+  title2.innerHTML = Ticket[selectedTicket].subTitle1;
+  title3.innerHTML = Ticket[selectedTicket].subTitle2;
+  fotterText.innerHTML = Ticket[selectedTicket].subText;
+  localStorage.setItem("selectedTicket", selectedTicket);
+  document.querySelector(".form-control").selectedIndex =
+    Number(localStorage.getItem("selectedTicket")) + 1;
+};
+const Ticket = [
+  {
+    title: "Normalny",
+    subTitle1: "JEDNORAZOWY DO 3 PRZYSTANKÓW",
+    subTitle2: "MZK JASTRZĘBIE-ZDRÓJ",
+    subText: `&nbsp; Bilet jednorazowy, na przejazd do 3 przystanków,
+  autobusemy wybranej linii. Ważny od momentu zakupu. <br />
+  Nie uprawnia do przesiadek.`,
+  },
+  {
+    title: "Normalny",
+    subTitle1: "JEDNORAZOWY 4 - 7 PRZYSTANKÓW",
+    subTitle2: "MZK JASTRZĘBIE-ZDRÓJ",
+    subText: `&nbsp; Bilet jednorazowy, na przejazd od 4 do 7 przystanków,
+autobusemy wybranej linii. Ważny od momentu zakupu. <br />
+Nie uprawnia do przesiadek.`,
+  },
+  {
+    title: "Normalny",
+    subTitle1: "JEDNORAZOWY 8 - 12 PRZYSTANKÓW",
+    subTitle2: "MZK JASTRZĘBIE-ZDRÓJ",
+    subText: `&nbsp; Bilet jednorazowy, na przejazd od 8 do 12 przystanków,
+autobusemy wybranej linii. Ważny od momentu zakupu. <br />
+Nie uprawnia do przesiadek.`,
+  },
+  {
+    title: "Normalny",
+    subTitle1: "JEDNORAZOWY 13 - 18 PRZYSTANKÓW",
+    subTitle2: "MZK JASTRZĘBIE-ZDRÓJ",
+    subText: `&nbsp; Bilet jednorazowy, na przejazd od 13 do 18 przystanków,
+autobusemy wybranej linii. Ważny od momentu zakupu. <br />
+Nie uprawnia do przesiadek.`,
+  },
+  {
+    title: "Normalny",
+    subTitle1: "JEDNORAZOWY POWYŻEJ 18 PRZYSTANKÓW",
+    subTitle2: "MZK JASTRZĘBIE-ZDRÓJ",
+    subText: `&nbsp; Bilet jednorazowy, na przejazd powyżej 18 przystanków,
+autobusemy wybranej linii. Ważny od momentu zakupu. <br />
+Nie uprawnia do przesiadek.`,
+  },
+  {
+    title: "Ulgowy",
+    subTitle1: "JEDNORAZOWY DO 3 PRZYSTANKÓW",
+    subTitle2: "MZK JASTRZĘBIE-ZDRÓJ",
+    subText: `&nbsp; Bilet jednorazowy, na przejazd do 3 przystanków,
+  autobusemy wybranej linii. Ważny od momentu zakupu. <br />
+  Nie uprawnia do przesiadek.`,
+  },
+  {
+    title: "Ulgowy",
+    subTitle1: "JEDNORAZOWY 4 - 7 PRZYSTANKÓW",
+    subTitle2: "MZK JASTRZĘBIE-ZDRÓJ",
+    subText: `&nbsp; Bilet jednorazowy, na przejazd od 4 do 7 przystanków,
+autobusemy wybranej linii. Ważny od momentu zakupu. <br />
+Nie uprawnia do przesiadek.`,
+  },
+  {
+    title: "Ulgowy",
+    subTitle1: "JEDNORAZOWY 8 - 12 PRZYSTANKÓW",
+    subTitle2: "MZK JASTRZĘBIE-ZDRÓJ",
+    subText: `&nbsp; Bilet jednorazowy, na przejazd od 8 do 12 przystanków,
+autobusemy wybranej linii. Ważny od momentu zakupu. <br />
+Nie uprawnia do przesiadek.`,
+  },
+  {
+    title: "Ulgowy",
+    subTitle1: "JEDNORAZOWY 13 - 18 PRZYSTANKÓW",
+    subTitle2: "MZK JASTRZĘBIE-ZDRÓJ",
+    subText: `&nbsp; Bilet jednorazowy, na przejazd od 13 do 18 przystanków,
+autobusemy wybranej linii. Ważny od momentu zakupu. <br />
+Nie uprawnia do przesiadek.`,
+  },
+  {
+    title: "Ulgowy",
+    subTitle1: "JEDNORAZOWY POWYŻEJ 18 PRZYSTANKÓW",
+    subTitle2: "MZK JASTRZĘBIE-ZDRÓJ",
+    subText: `&nbsp; Bilet jednorazowy, na przejazd powyżej 18 przystanków,
+autobusemy wybranej linii. Ważny od momentu zakupu. <br />
+Nie uprawnia do przesiadek.`,
+  },
+];
